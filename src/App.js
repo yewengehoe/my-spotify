abab93d2b21a3d8a43340a5a9e463b7273366247
Eng Hoe <yewengehoe@gmail.com>
2020-05-22 20:58:07 +0800

    First Working API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Spotify from 'spotify-web-api-js';
 
-function App() {
+const spotifyWebApi = new Spotify();
+
+class App extends Component {
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: {
+        name: "Not Checked",
+        image: ''
+      }
+    }
+    if (params.access_token){
+      spotifyWebApi.setAccessToken(params.access_token)
+    }
+  }
+  
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1); // eslint-disable-next-line 
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+getNowPlaying(){
+  spotifyWebApi.getMyCurrentPlaybackState()
+    .then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          image: response.item.album.images[0].url
+        }
+      })
+  })
+}
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href = 'http://localhost:8888'>
+        <button>Login with Spotify</button>
+      </a>
+      <div>Now Playing: {this.state.nowPlaying.name} </div>
+      <div>
+        <img src={this.state.nowPlaying.image} alt='No album cover to show.' style={{width: 200}}/>
+      </div>
+      <button onClick={() => this.getNowPlaying()}>
+        Check Now Playing
+      </button>
     </div>
   );
+}
 }
 
 export default App;
